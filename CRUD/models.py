53da6ec50a77abe7632e47171c66f090ecf75185
divon54 <divonligare54@gmail.com>
2024-11-25 16:58:21 +0300

    djangoCRUD

---
+++ CRUD/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=30, blank=False, null=False)
+    email = models.EmailField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length=30, blank=False, null=False)
+
+
+def __str__(self):
+    return self.name
+
